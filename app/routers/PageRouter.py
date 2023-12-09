17b56727987da17ce0aaae098151838bc2df94b1
Maxim Azarov <az.maks.work@mail.ru>
2023-12-10 02:41:27 +0300

    create page

--- app/routers/PageRouter.py
+++ app/routers/PageRouter.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+import datetime
 
-router = APIRouter(prefix="")
+from app.services.TableService import TableService
+
+router = APIRouter(prefix="/page")
 
 
 templates = Jinja2Templates(directory="app/templates")
@@ -11,4 +14,8 @@ templates = Jinja2Templates(directory="app/templates")
 @router.get("/auth", response_class=HTMLResponse)
 async def get_auth_page(request: Request):
     return templates.TemplateResponse("auth.html", {"request": request})
+
+@router.get("/tables", response_class=HTMLResponse)
+async def get_auth_page(request: Request):
+    return templates.TemplateResponse("tables.html", {"request": request})
     

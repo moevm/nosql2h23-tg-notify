e03aab01d8892fe1f768443f41f20c9acb4160d7
Maxim Azarov <az.maks.work@mail.ru>
2023-12-10 15:14:13 +0300

    add token check

--- app/routers/PageRouter.py
+++ app/routers/PageRouter.py
@@ -1,9 +1,10 @@
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 import datetime
 
 from app.services.TableService import TableService
+from app.services.AuthService import AuthService
 
 router = APIRouter(prefix="/page")
 
@@ -15,7 +16,9 @@ templates = Jinja2Templates(directory="app/templates")
 async def get_auth_page(request: Request):
     return templates.TemplateResponse("auth.html", {"request": request})
 
-@router.get("/tables", response_class=HTMLResponse)
+@router.get("/tables",
+            dependencies=[Depends(AuthService.validate_token)], 
+            response_class=HTMLResponse)
 async def get_auth_page(request: Request):
     return templates.TemplateResponse("tables.html", {"request": request})
     
